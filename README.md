5b2eb5b1656ce473209b41d1c54ca88aebe4fe96
Sébastien Saunier <seb@saunier.me>
2019-10-06 19:17:29 +0200

    WIP

---
+++ README.md
@@ -0,0 +1,16 @@
+# Data Science Bootcamp Setup
+
+<table>
+  <tr>
+    <td>
+      <a href="macOS.md">
+        <img src="images/apple.png" alt="macOS" />
+      </a>
+    </td>
+    <td>
+      <a href="WINDOWS.md">
+        <img src="images/windows.png" alt="Windows">
+      </a>
+    </td>
+  </tr>
+</table>
--- build.rb
+++ build.rb
@@ -1,13 +1,20 @@
+#!/usr/bin/env ruby -wU
+
 WINDOWS = %w[win_anaconda win_sublime win_git_bash].freeze
+# MAC_OS = %w[setup/intro osx_command_line_tools github homebrew osx_sublime_text osx_oh_my_zsh github_rsa dotfiles ssh_osx rbenv_osx rbenv_ruby osx_postgresql osx_security checkup alumni_platform osx_slack osx_preferencesosx_command_line_tools github homebrew osx_sublime_text osx_oh_my_zsh github_rsa dotfiles ssh_osx rbenv_osx rbenv_ruby osx_postgresql osx_security checkup alumni_platform osx_slack osx_preferences].freeze
+MAC_OS = %w[setup/intro].freeze
 
 filenames = {
-  'WINDOWS.md' => WINDOWS
+  'WINDOWS.md' => WINDOWS,
+  'macOS.md' => MAC_OS
 }
 
 filenames.each do |filename, partials|
   File.open(filename.to_s, 'w:utf-8') do |f|
     partials.each do |partial|
-      f << File.read(File.join('_partials', "#{partial}.md"), encoding: "utf-8")
+      match_data = partial.match(/setup\/(?<partial>[a-z_]+)/)
+      file = match_data ? File.join('../setup', '_partials', "#{match_data[:partial]}.md") : File.join('_partials', "#{partial}.md")
+      f << File.read(file, encoding: 'utf-8')
       f << "\n\n"
     end
   end
