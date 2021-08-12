8be7fd2fc2d8aedd881a0b6360d133334e497dbe
kevin robert <kevin.j.robert@gmail.com>
2021-08-12 15:35:42 +0200

    missing coma

--- build.rb
+++ build.rb
@@ -3,7 +3,7 @@
 CONSTANTS = {
     'PYTHON_VERSION' => "3.9.6",
     'REQUIREMENTS_URL' => "https://raw.githubusercontent.com/lewagon/data-runner/py-3.9.6-pandas-1.3/requirements.txt",
-    'PIP_CHECKER_URL' => "https://gist.githubusercontent.com/krokrob/2e5a61b20582b55bbb034c4ea1e9f633/raw/95fba0d6430682a682dd92409b0878c9edf6e125/pip_check.sh"
+    'PIP_CHECKER_URL' => "https://gist.githubusercontent.com/krokrob/2e5a61b20582b55bbb034c4ea1e9f633/raw/95fba0d6430682a682dd92409b0878c9edf6e125/pip_check.sh",
     'PIP_LOADER_URL' => "https://gist.githubusercontent.com/krokrob/90e35dee7ed2b20852b099331510b369/raw/09178c49db6e7537eed68335a25fbb00c7ca1fd4/pip_check.py"
 }
 
