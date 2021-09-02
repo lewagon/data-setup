1a0716393a2d1c45b61875a30cf81ea7703f82f1
kevin robert <kevin.j.robert@gmail.com>
2021-09-02 11:17:40 +0200

    store python version in a txt file

--- checks/python_checker.rb
+++
@@ -1,8 +0,0 @@
-require_relative '../config.rb'
-
-python_version = %x[python -V]
-if python_version =~ /#{CONSTANTS["PYTHON_VERSION"]}/
-  puts "✅ Your Python version OK."
-else
-  puts "❌ Your Python version is #{python_version.match(/(\d\.\d\.\d)/)[0]} but it should be #{CONSTANTS['PYTHON_VERSION']}."
-end
--- config.rb
+++
@@ -1,8 +0,0 @@
-CONSTANTS = {
-    'PYTHON_VERSION' => "3.9.6",
-    'REQUIREMENTS_URL' => "https://raw.githubusercontent.com/lewagon/data-runner/py-3.9.6-pandas-1.3/requirements.txt",
-    'CONFIG_URL' => "https://raw.githubusercontent.com/lewagon/data-setup/master/config.rb",
-    'PYTHON_CHECKER_URL' => "https://raw.githubusercontent.com/lewagon/data-setup/master/checks/python_checker.rb",
-    'PIP_CHECKER_URL' => "https://raw.githubusercontent.com/lewagon/data-setup/master/checks/pip_check.sh",
-    'PIP_LOADER_URL' => "https://raw.githubusercontent.com/lewagon/data-setup/master/checks/pip_check.py"
-}
