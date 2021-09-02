457e20c36c55c4ee196c0fc74c983795f6c61176
Gaetan Manchon <gmanchon@gmail.com>
2021-09-02 13:45:59 +0200

    run check scripts without saving to files

--- checks/python_checker.sh
+++ checks/python_checker.sh
@@ -1,7 +1,5 @@
-python_version=$(head -1 python_version.txt)
-python_version=${python_version%"\n"}
 my_python_version=$(python -V)
-if [[ $my_python_version = *$python_version* ]];then
+if [[ $my_python_version = *$0* ]];then
   echo "✅ Your Python version OK."
 else
   echo "❌ Your Python version is $my_python_version but it should be $python_version."
