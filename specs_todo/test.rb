63de09e60b22915aa04aca5a88c81013c62c73ca
Gaetan Manchon <gmanchon@gmail.com>
2022-01-06 16:32:55 +0100

    remove specs todo

--- specs_todo/test.rb
+++
@@ -1,9 +0,0 @@
-require 'yaml'
-
-specs_path = File.join('test.yml')
-
-package_confs = YAML.load_file(specs_path)
-
-puts "package_confs #{package_confs["setup"]}"
-
-puts "\nflatten #{package_confs["setup"].flatten}"
